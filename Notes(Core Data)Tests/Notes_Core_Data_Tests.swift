b11f8266d1b7c05a8346dde277bdfab2ababb463
Kavya Prajapati <kavya8958p@gmail.com>
2023-01-13 13:57:33 +0530

    Initial Commit

---
+++ Notes(Core Data)Tests/Notes_Core_Data_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Notes_Core_Data_Tests.swift
+//  Notes(Core Data)Tests
+//
+//  Created by Sunil Developer on 13/01/23.
+//
+
+import XCTest
+@testable import Notes_Core_Data_
+
+class Notes_Core_Data_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
